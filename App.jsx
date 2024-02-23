1a5b4228d4896deebadbeea9ab6bf3aff5e78822
clamcatfish <clam0540@gmail.com>
2024-02-22 20:44:03 -0700

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,48 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+import {StyleSheet, Text, View} from 'react-native';
+import ToDoList from './components/ToDoList';
+import ToDoForm from './components/ToDoForm';
+
+function App() {
+  return (
+    <View>
+      <ToDoList />
+      <ToDoForm />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+});
+
+export default App;
